0cf4cf340b91ab31cbd34a96ebf013f4114a4e50
Phantom <humanoidphantom@gmail.com>
2018-01-06 21:07:21 +0300

    check hash

--- quake.py
+++ quake.py
@@ -41,9 +41,12 @@ class Quake:
                 self.nodes = response.json()['nodes']
                 self.blockchain.chain = response.json()['chain']
 
+    def generate_node_hash(self, pubkey, node_host, node_port):
+        return hashlib.sha256('%s%s%s' % (pubkey, self.host, self.port))
+
     def generate_identity(self):
         pubkey = self.key.publickey()
-        node_hash = hashlib.sha256('%s%s%s' % (pubkey, self.host, self.port))
+        node_hash = self.generate_node_hash(pubkey, self.host, self.port)
         return {
             'hash': node_hash,
             'pubkey': pubkey,
@@ -53,12 +56,23 @@ class Quake:
     def sort_nodes(self):
         self.nodes = sorted(self.nodes, key=itemgetter('hash'))
 
+    def check_hash(self, identity):
+        node_hash = self.generate_node_hash(identity['pubkey'], identity['node_host'], identity['node_port'])
+
+        return node_hash == identity['hash']
+
 
 app = Flask(__name__)
 
 node_identifier = str(uuid4()).replace('-', '')
 
 quake = Quake()
+
+
+def check_required(required, received):
+    if not all(k in received for k in required):
+        return 'Missing values', 400
+    return -1
 
 
 @app.route('/data', methods=['GET'])
@@ -76,8 +90,9 @@ def tx():
 
     required = ['sender', 'receiver', 'amount']
 
-    if not all(k in values for k in required):
-        return 'Missing values', 400
+    check_result = check_required(required, values)
+    if check_result != -1:
+        return check_result
 
     index = quake.blockchain.new_transaction(values['sender'], values['receiver'], values['amount'])
 
@@ -87,12 +102,28 @@ def tx():
     return jsonify(response), 200
 
 
+@app.route('/neighbor', methods=['POST'])
+def neighbor():
+    # Receive node's identity. Become a neighbor
+    values = request.get_json()
+    required = ['hash', 'pubkey', 'host', 'port']
+
+    check_result = check_required(required, values)
+    if check_result != -1:
+        return check_result
+
+
+
+    response = {}
+    return jsonify(response), 200
+
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
 
-    parser.add_argument('-H', '--host', default='127.0.0.1', type=str, help='host or url')
+    parser.add_argument('-H', '--host', default='127.0.0.1', type=str, help='ip or url')
     parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
 
     parser.add_argument('-cH', '--connect_to_host', type=str, help='host to connect to')
