6d62019f6b5dfd5d422915add400bb262576bc5b
Phantom <humanoidphantom@gmail.com>
2018-01-10 18:54:50 +0300

    fix

--- quake.py
+++ quake.py
@@ -359,7 +359,7 @@ def start():
     parser = ArgumentParser()
 
     parser.add_argument('-H', '--host', default='127.0.0.1', type=str, help='ip or url')
-    parser.add_argument('-p', '--port', default=49001, type=int, help='port to listen on')
+    parser.add_argument('-p', '--port', default=50001, type=int, help='port to listen on')
 
     parser.add_argument('-cH', '--connect_to_host', type=str, help='host to connect to')
     parser.add_argument('-cp', '--connect_to_port', type=int, help='port to connect to')
@@ -374,8 +374,8 @@ def start():
     connect_port = args.connect_to_port
 
     host = args.host
-    port = args.port
-    main.port = str(args.port + Quake.PORT_DISTANCE)
+    port = args.port - Quake.PORT_DISTANCE
+    main.port = str(args.port)
 
     main.run()
 
